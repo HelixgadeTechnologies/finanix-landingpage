5275d49ba530b7fc1ab62286cc13e23cb614ba82
ogiamien emmanuel tammy <ogiamien222@gmail.com>
2023-09-27 12:27:57 +0100

    modified:   components/Layout.js         new file:   pages/confirmation.js         modified:   pages/modalset.js         modified:   tailwind.config.js         modified:   yarn.lock

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 import { useRouter } from 'next/router';
 
-const noLayout = ['/login', '/signup', '/dashboard', '/profile', '/trades','/settings', '/compliance'];
+const noLayout = ['/login', '/signup', '/dashboard', '/profile', '/trades','/settings', '/compliance', '/confirmation'];
 
 export default function Layout({ children }) {
   const router = useRouter();
